3b252a0db536f61e6c4ed4fa2a6fa5e6d89529d0
DrCosinus <minwene@free.fr>
2018-05-06 17:51:38 +0200

    strong type lib wip: - get rid of constants in modifiers structs - improving tests

--- strong_type.h
+++ strong_type.h
@@ -36,11 +36,39 @@ constexpr TUPLE operator-(const TUPLE& _lhs, const TUPLE& _rhs)
 
 namespace wit
 {
-    template<typename VALUE_TYPE, typename TAG_TYPE, typename... MODIFIER_TYPES>
+    struct equalable {}; // operators == and != only (need only operator == on the underlying type)
+    struct comparable {}; // operators == , != , <, >, <= and >= (need only operator == and < on the underlying type)
+    struct self_addable {};
+    struct self_subtractable {};
+    struct self_multipliable {};
+    struct self_dividable {};
+    struct incrementable {};
+    struct decrementable {};
+    struct stringable {};
+    struct convertible_to_value {}; // explicit conversion operator to types in which the value can be converted to
+    struct signable {};
+
+    namespace detail
+    {
+        template <typename U> struct is_equalable : std::conditional_t<std::is_base_of_v<equalable, U>, std::true_type, std::false_type> {};
+        template <typename U> struct is_comparable : std::conditional_t<std::is_base_of_v<comparable, U>, std::true_type, std::false_type> {};
+        template <typename U> struct is_self_addable : std::conditional_t<std::is_base_of_v<self_addable, U>, std::true_type, std::false_type> {};
+        template <typename U> struct is_self_subtractable : std::conditional_t<std::is_base_of_v<self_subtractable, U>, std::true_type, std::false_type> {};
+        template <typename U> struct is_self_multipliable : std::conditional_t<std::is_base_of_v<self_multipliable, U>, std::true_type, std::false_type> {};
+        template <typename U> struct is_self_dividable : std::conditional_t<std::is_base_of_v<self_dividable, U>, std::true_type, std::false_type> {};
+        template <typename U> struct is_incrementable : std::conditional_t<std::is_base_of_v<incrementable, U>, std::true_type, std::false_type> {};
+        template <typename U> struct is_decrementable : std::conditional_t<std::is_base_of_v<decrementable, U>, std::true_type, std::false_type> {};
+        template <typename U> struct is_stringable : std::conditional_t<std::is_base_of_v<stringable, U>, std::true_type, std::false_type> {};
+        template <typename U> struct is_convertible_to_value : std::conditional_t<std::is_base_of_v<convertible_to_value, U>, std::true_type, std::false_type> {};
+        template <typename U> struct is_signable : std::conditional_t<std::is_base_of_v<signable, U>, std::true_type, std::false_type> {};
+    } // namespace detail
+
+    // DERIVED_TYPE = at first it was a unique tag (thank to CRTP) but now we use it to cast the result of some operations into this type
+    template<typename VALUE_TYPE, typename DERIVED_TYPE, typename... MODIFIER_TYPES>
     struct strong_type : MODIFIER_TYPES...
     {
         using value_type = VALUE_TYPE;
-        using tag_type = TAG_TYPE;
+        using derived_type = DERIVED_TYPE;
 
 // constructors
         constexpr explicit strong_type(value_type _value) : value_(std::move(_value)) {}
@@ -52,85 +80,78 @@ namespace wit
 
 // helpers
     private:
-        template<bool B> using int_if = std::enable_if_t<B, int>;
+        template<typename U, template<typename> class... PREDICATES>
+        static constexpr bool any_predicate_of() { return (... || PREDICATES<U>::value); }
+        template<typename U, template<typename> class... PREDICATES> using check = std::enable_if_t<any_predicate_of<U, PREDICATES...>()>;
     public:
+
 // convertion
-        template<typename U = strong_type, int_if<U::is_explicitly_convertible_to_value> = 0>
-        constexpr explicit operator value_type() const { return value_; }
+        // template<typename U = strong_type, typename = check<U, detail::is_convertible_to_value>>
+        // constexpr explicit operator value_type() const { return value_; }
+        template<typename T, typename U = strong_type, typename = check<U, detail::is_convertible_to_value>, typename = std::enable_if_t<std::is_convertible_v<value_type, T>>>
+        constexpr explicit operator T() const { return T(value_); }
 
 // comparison
-        template<typename U = strong_type, int_if<U::is_equalable> = 0>
-        constexpr bool operator==(const tag_type& _rhs) const { return value_ == _rhs.value_; } // should eventually consider using !(a<b || b<a) to not use operator ==
-        template<typename U = strong_type, int_if<U::is_comparable> = 0>
-        constexpr bool operator==(const tag_type& _rhs) const { return value_ == _rhs.value_; } // should eventually consider using !(a<b || b<a) to not use operator ==
-        template<typename U = strong_type, int_if<U::is_equalable> = 0>
-        constexpr bool operator!=(const tag_type& _rhs) const { return  !(tag_object() == _rhs); }
-        template<typename U = strong_type, int_if<U::is_comparable> = 0>
-        constexpr bool operator!=(const tag_type& _rhs) const { return  !(tag_object() == _rhs); }
-        template<typename U = strong_type, int_if<U::is_comparable> = 0>
-        constexpr bool operator<(const tag_type& _rhs) const { return  value_ < _rhs.value_; }
-        template<typename U = strong_type, int_if<U::is_comparable> = 0>
-        constexpr bool operator>(const tag_type& _rhs) const { return  _rhs < tag_object(); }
-        template<typename U = strong_type, int_if<U::is_comparable> = 0>
-        constexpr bool operator<=(const tag_type& _rhs) const { return  !(tag_object() > _rhs); }
-        template<typename U = strong_type, int_if<U::is_comparable> = 0>
-        constexpr bool operator>=(const tag_type& _rhs) const { return  !(tag_object() < _rhs); }
+        template<typename U = strong_type, typename = check<U, detail::is_equalable, detail::is_comparable>>
+        constexpr bool operator==(const derived_type& _rhs) const { return value_ == _rhs.value_; } // should eventually consider using !(a<b || b<a) to not use operator ==
+        template<typename U = strong_type, typename = check<U, detail::is_equalable, detail::is_comparable>>
+        constexpr bool operator!=(const derived_type& _rhs) const { return  !(derived_object() == _rhs); }
+        template<typename U = strong_type, typename = check<U, detail::is_comparable>>
+        constexpr bool operator<(const derived_type& _rhs) const { return  value_ < _rhs.value_; }
+        template<typename U = strong_type, typename = check<U, detail::is_comparable>>
+        constexpr bool operator>(const derived_type& _rhs) const { return  _rhs < derived_object(); }
+        template<typename U = strong_type, typename = check<U, detail::is_comparable>>
+        constexpr bool operator<=(const derived_type& _rhs) const { return  !(derived_object() > _rhs); }
+        template<typename U = strong_type, typename = check<U, detail::is_comparable>>
+        constexpr bool operator>=(const derived_type& _rhs) const { return  !(derived_object() < _rhs); }
 
 // arithmetic
-        template<typename U = strong_type, int_if<U::is_self_addable> = 0>
-        constexpr auto operator+(const tag_type& _rhs) const { return tag_type{ value_ + _rhs.value_ }; }
-        template<typename U = strong_type, int_if<U::is_self_substractable> = 0>
-        constexpr auto operator-(const tag_type& _rhs) const { return tag_type{ value_ - _rhs.value_ }; }
-        template<typename U = strong_type, int_if<U::has_unary_sign_operators> = 0>
-        constexpr auto operator+() const { return tag_object(); }
-        template<typename U = strong_type, int_if<U::has_unary_sign_operators> = 0>
-        constexpr auto operator-() const { return tag_type{ -value_ }; }
+        template<typename U = strong_type, typename = check<U, detail::is_self_addable>>
+        constexpr auto operator+(const derived_type& _rhs) const { return derived_type{ value_ + _rhs.value_ }; }
+        template<typename U = strong_type, typename = check<U, detail::is_self_subtractable>>
+        constexpr auto operator-(const derived_type& _rhs) const { return derived_type{ value_ - _rhs.value_ }; }
+        template<typename U = strong_type, typename = check<U, detail::is_signable>>
+        constexpr auto operator+() const { return derived_object(); }
+        template<typename U = strong_type, typename = check<U, detail::is_signable>>
+        constexpr auto operator-() const { return derived_type{ -value_ }; }
 
-        template<typename U = strong_type, int_if<U::is_incrementable> = 0>
-        constexpr decltype(auto) operator++() { ++value_; return tag_object(); }
-        template<typename U = strong_type, int_if<U::is_incrementable> = 0>
-        constexpr auto operator++(int) { auto res = tag_object(); ++value_; return res; }
-        template<typename U = strong_type, int_if<U::is_incrementable> = 0>
-        constexpr decltype(auto) operator+=(const tag_type& _rhs) { value_ += _rhs.value_; return tag_object(); }
+        template<typename U = strong_type, typename = check<U, detail::is_incrementable>>
+        constexpr decltype(auto) operator++() { ++value_; return derived_object(); }
+        template<typename U = strong_type, typename = check<U, detail::is_incrementable>>
+        constexpr auto operator++(int) { auto res = derived_object(); ++value_; return res; }
+        template<typename U = strong_type, typename = check<U, detail::is_incrementable>>
+        constexpr decltype(auto) operator+=(const derived_type& _rhs) { value_ += _rhs.value_; return derived_object(); }
 
-        template<typename U = strong_type, int_if<U::is_decrementable> = 0>
-        constexpr decltype(auto) operator--() { --value_; return tag_object(); }
-        template<typename U = strong_type, int_if<U::is_decrementable> = 0>
-        constexpr auto  operator--(int) { auto res = tag_object(); --value_; return res; }
-        template<typename U = strong_type, int_if<U::is_decrementable> = 0>
-        constexpr decltype(auto) operator-=(const tag_type& _rhs) { value_ -= _rhs.value_; return tag_object(); }
+        template<typename U = strong_type, typename = check<U, detail::is_decrementable>>
+        constexpr decltype(auto) operator--() { --value_; return derived_object(); }
+        template<typename U = strong_type, typename = check<U, detail::is_decrementable>>
+        constexpr auto  operator--(int) { auto res = derived_object(); --value_; return res; }
+        template<typename U = strong_type, typename = check<U, detail::is_decrementable>>
+        constexpr decltype(auto) operator-=(const derived_type& _rhs) { value_ -= _rhs.value_; return derived_object(); }
+
+        template<typename U = strong_type, typename = check<U, detail::is_self_multipliable>>
+        constexpr auto operator*(const derived_type& _rhs) const { return derived_type{ value_ * _rhs.value_ }; }
+
+        template<typename U = strong_type, typename = check<U, detail::is_self_dividable>>
+        constexpr auto operator/(const derived_type& _rhs) const { return derived_type{ value_ / _rhs.value_ }; }
+
 
 // to_string
-        template<typename U = strong_type, int_if<U::is_stringable> = 0>
+        template<typename U = strong_type, typename = check<U, detail::is_stringable>>
         std::string to_string() const { return std::to_string( value_ ); }
 
     protected:
-        constexpr tag_type& tag_object() { return static_cast<tag_type&>(*this); }
-        constexpr const tag_type& tag_object() const { return static_cast<const tag_type&>(*this); }
+        constexpr derived_type& derived_object() { return static_cast<derived_type&>(*this); }
+        constexpr const derived_type& derived_object() const { return static_cast<const derived_type&>(*this); }
         constexpr const value_type& get_value() const { return value_; }
     private:
         value_type value_;
     };
-
-    // operators == and != only (need only operator == on the underlying type)
-    struct equalable            { static constexpr bool is_equalable = true; };
-    // operators == , != , <, >, <= and >= (need only operator == and < on the underlying type)
-    struct comparable           { static constexpr bool is_comparable = true; };
-    struct self_addable         { static constexpr bool is_self_addable = true; };
-    struct self_subtractable    { static constexpr bool is_self_substractable = true; };
-    struct self_multipliable    { static constexpr bool is_self_multipliable = true; }; // no yet implemented
-    struct self_dividable       { static constexpr bool is_self_dividable = true; }; // no yet implemented
-    struct incrementable        { static constexpr bool is_incrementable = true; };
-    struct decrementable        { static constexpr bool is_decrementable = true; };
-    struct stringable           { static constexpr bool is_stringable = true; };
-    // explicit convertion
-    struct convertible_to_value { static constexpr bool is_explicitly_convertible_to_value = true; };
-    struct unary_sign           { static constexpr bool has_unary_sign_operators = true; };
 } // namespace wit
 
 namespace std
 {
-    template<typename U, std::enable_if_t<U::is_stringable, int> = 0>
+    template<typename U, std::enable_if_t<wit::detail::is_stringable<U>::value, int> = 0>
     string to_string(const U& _strongly_typed_object)
     {
         return _strongly_typed_object.to_string();
--- strong_type_tests.cpp
+++ strong_type_tests.cpp
@@ -3,174 +3,207 @@
 
 #include "strong_type.h"
 
-struct minimal_strong_type : wit::strong_type<unsigned long long, minimal_strong_type>
-{
-    using strong_type::strong_type;
-    using strong_type::get_value;
-};
-
-TEST_CASE( "", "")
-{
-    minimal_strong_type foo{ 1234 };
-    REQUIRE( foo.get_value() == 1234 );
-}
-
-// safe length unit => value is in meter (should be carved in the code, not in a comment)
-struct length_unit : wit::strong_type<
-    float           // value type
-    , length_unit   // unique tag (thank to CRTP, it is the type itself) moreover the result of some operations will be cast to it
-    , wit::comparable
-    , wit::convertible_to_value
-    , wit::self_addable, wit::self_subtractable, wit::unary_sign
-    , wit::stringable
-    , wit::incrementable, wit::decrementable
-    >
-{
-    using strong_type::strong_type;
-    using strong_type::get_value; // bring get_value member function publicly for test purpose only
-};
-
-length_unit operator""_m(long double _meters) { return length_unit{float(_meters)}; }
-length_unit operator""_m(unsigned long long _meters) { return length_unit{float(_meters)}; }
-
-TEST_CASE( "Strong type: length_unit", "[strong_type]" )
-{
-    SECTION( "Explicit construction from underlying type, unary plus/minus operator and custom literal operators" )
-    {
-        REQUIRE( length_unit{ 32 }.get_value() == 32 );
-        REQUIRE( length_unit{ 17 } == 17_m);
-        REQUIRE( 17_m == length_unit{ 17 });
-        REQUIRE( +17_m == length_unit{ 17 });
-        REQUIRE( -17_m == -length_unit{ 17 });
-        REQUIRE( +17_m == length_unit{ 17 });
-        REQUIRE( -509_m == length_unit{ -509 });
-    }
-    SECTION( "Comparison operators" )
-    {
-        REQUIRE_FALSE( -17_m == 42_m );
-        REQUIRE( -17_m != 42_m );
-        REQUIRE( -17_m < 42_m );
-        REQUIRE_FALSE( -17_m > 42_m );
-        REQUIRE( -17_m <= 42_m );
-        REQUIRE_FALSE( -17_m >= 42_m );
-
-        REQUIRE_FALSE( 42_m == -17_m );
-        REQUIRE( 42_m != -17_m );
-        REQUIRE_FALSE( 42_m < -17_m );
-        REQUIRE( 42_m > -17_m );
-        REQUIRE_FALSE( 42_m <= -17_m );
-        REQUIRE( 42_m >= -17_m );
-
-        REQUIRE( 23_m == 23_m );
-        REQUIRE_FALSE( 23_m != 23_m );
-        REQUIRE_FALSE( 23_m <  23_m );
-        REQUIRE_FALSE( 23_m >  23_m );
-        REQUIRE( 23_m <= 23_m );
-        REQUIRE( 23_m >= 23_m );
-    }
-    SECTION( "Explicit convertion to value type" )
-    {
-        REQUIRE( float(5_m) == 5 );
-    }
-    SECTION( "Arithmetic operators" )
-    {
-        REQUIRE( 4_m + 7_m == 11_m );
-        REQUIRE( 24_m - 11_m == 13_m );
-    }
-    SECTION( "std::to_string")
-    {
-        REQUIRE( std::to_string(18_m) == std::to_string(18.f));
-    }
-    SECTION( "Incrementation operators")
-    {
-        auto t = 37_m;
-        REQUIRE( ++t == 38_m );
-        REQUIRE( t++ == 38_m );
-        REQUIRE( t == 39_m );
-        t += 17_m;
-        REQUIRE( t == 56_m );
-        (++((t += 3_m) += 5_m))++;
-        REQUIRE( t == 66_m );
-    }
-}
-
-// a 3d vector without any semantic
-template<typename T>
-struct vector3
-{
-    vector3(T _x, T _y, T _z) : x{std::move(_x)}, y{std::move(_y)}, z{std::move(_z)} {}
-    bool operator==(const vector3& _rhs) const { return x == _rhs.x && y == _rhs.y && z == _rhs.z; }
-    bool operator!=(const vector3& _rhs) const { return !(*this == _rhs); }
-    vector3 operator+(const vector3& _rhs) const { return { x + _rhs.x, y + _rhs.y, z + _rhs.z }; }
-    T x, y, z;
-};
-
-TEST_CASE( "Strong type: vector3<length_unit>", "[strong_type]" )
-{
-    auto v0 = vector3<length_unit>{ 1_m, 2_m, 3_m };
-    auto v1 = vector3<length_unit>{ 1_m, 2_m, 3_m };
-    auto v2 = vector3<length_unit>{ 4_m, 5_m, 6_m };
-
-    REQUIRE(v0==v1);
-    REQUIRE(v1!=v2);
-}
-
 #include <tuple>
 
 TEST_CASE( "Tuple helper", "[Tuple helpers]")
 {
-    {
-        auto sum = std::make_tuple(1,2,3) + std::make_tuple(10,20,30);
-        REQUIRE(std::get<0>(sum)==11);
-        REQUIRE(std::get<1>(sum)==22);
-        REQUIRE(std::get<2>(sum)==33);
-    }
-    {
-        auto x1 = 1_m;
-        auto p1 = std::make_tuple( 1_m, 2_m, 3_m);
-        auto sum = std::make_tuple(1_m,2_m,3_m) + std::make_tuple(10_m,20_m,30_m);
-        REQUIRE(std::get<0>(sum)==11_m);
-        REQUIRE(std::get<1>(sum)==22_m);
-        REQUIRE(std::get<2>(sum)==33_m);
-    }
+    auto sum = std::make_tuple(1,2,3) + std::make_tuple(10,20,30);
+    REQUIRE(std::get<0>(sum)==11);
+    REQUIRE(std::get<1>(sum)==22);
+    REQUIRE(std::get<2>(sum)==33);
 }
 
-// strong type aggregate tentative
-namespace experimental
+namespace test_minimal
 {
-    template<typename T>
-    struct vector3 : wit::strong_type<std::tuple<T, T, T>, vector3<T>, wit::equalable, wit::self_addable, wit::self_subtractable>
+    struct minimal : wit::strong_type<unsigned long long, minimal>
     {
-        //using super = wit::strong_type<std::tuple<T, T, T>, vector3<T>, wit::equalable, wit::self_addable, wit::self_subtractable>;
-        using wit::strong_type<std::tuple<T, T, T>, vector3<T>, wit::equalable, wit::self_addable, wit::self_subtractable>::strong_type;
-        //using strong_type::strong_type;
-        //vector3(T _x, T _y, T _z) : super{ std::make_tuple(std::move(_x), std::move(_y), std::move(_z)) } {}
+        using strong_type::strong_type;
+        using strong_type::get_value; // for test purpose
     };
-} // namespace experimental
 
-TEST_CASE( "Strong type: experimental::vector3<length_unit>", "[strong_type]" )
+    TEST_CASE( "Minimal strong type", "[strong type]")
+    {
+        minimal foo{ 1234 };
+        REQUIRE( foo.get_value() == 1234 );
+    }
+} // namespace test_minimal
+
+namespace test_comparable
 {
-    using vec3 = experimental::vector3<length_unit>;
+    struct foo : wit::strong_type<unsigned long long, foo, wit::comparable>
+    {
+        using strong_type::strong_type;
+    };
 
-    auto v0 = vec3{ 1_m, 2_m, 3_m };
-    auto v1 = vec3{ 1_m, 2_m, 3_m };
-    auto v2 = vec3{ 3_m, 5_m, 7_m };
+    TEST_CASE( "Equatable/Comparable", "[strong type]")
+    {
+        foo a{12}, b{12}, c{42};
 
-    REQUIRE( v0==v1 );
-    REQUIRE( v1!=v2 );
-    REQUIRE( v1+v2 == vec3{ 4_m, 7_m, 10_m });
-    REQUIRE( v2-v1 == vec3{ 2_m, 3_m, 4_m });
-}
+        REQUIRE( a==b );
+        REQUIRE_FALSE( a==c );
+        REQUIRE( b!=c );
+        REQUIRE_FALSE( b!=a );
 
-TEST_CASE( "Check for the name of the type return by some operators", "Types")
+        REQUIRE_FALSE( a<b );
+        REQUIRE_FALSE( a>b );
+        REQUIRE( a<=b );
+        REQUIRE( a>=b );
+
+        REQUIRE( a<c );
+        REQUIRE_FALSE( a>c );
+        REQUIRE( a<=c );
+        REQUIRE_FALSE( a>=c );
+
+        REQUIRE_FALSE( c<a );
+        REQUIRE( c>a );
+        REQUIRE_FALSE( c<=a );
+        REQUIRE( c>=a );
+    }
+} // namespace test_comparable
+
+namespace test_self_arithmetic
 {
-    auto name = typeid(1_m).name();
-    REQUIRE(0==strcmp(name, typeid(1_m++).name()));
-    REQUIRE(0==strcmp(name, typeid(++1_m).name()));
-    REQUIRE(0==strcmp(name, typeid(1_m+=5_m).name()));
-    REQUIRE(0==strcmp(name, typeid(1_m--).name()));
-    REQUIRE(0==strcmp(name, typeid(--1_m).name()));
-    REQUIRE(0==strcmp(name, typeid(1_m-=5_m).name()));
-    REQUIRE(0==strcmp(name, typeid(1_m+5_m).name()));
-    REQUIRE(0==strcmp(name, typeid(1_m-5_m).name()));
-}
+    struct scalar : wit::strong_type<int, scalar, wit::signable, wit::self_addable, wit::self_subtractable, wit::self_multipliable, wit::self_dividable, wit::incrementable, wit::decrementable, wit::equalable>
+    {
+        using strong_type::strong_type;
+    };
+
+    scalar operator""_z(unsigned long long _value) { return scalar{_value}; }
+
+    TEST_CASE( "self arithmetic", "[strong type]" )
+    {
+        REQUIRE( scalar{-59} == -scalar{59} );
+        REQUIRE( scalar{3} + scalar{5} == scalar{8} );
+        REQUIRE( scalar{5} - scalar{3} == scalar{2} );
+        REQUIRE( scalar{5} * scalar{3} == scalar{15} );
+        REQUIRE( scalar{63} / scalar{7} == scalar{9} );
+
+        SECTION( "Incrementation operators")
+        {
+            auto t = scalar{ 37 };
+            REQUIRE( ++t == scalar{ 38 } );
+            REQUIRE( t++ == scalar{ 38 } );
+            REQUIRE( t == scalar{ 39 } );
+            t += scalar{ 17 };
+            REQUIRE( t == scalar{ 56 } );
+            (++((t += scalar{ 3 }) += scalar{ 5 }))++;
+            REQUIRE( t == scalar{ 66 } );
+        }
+    }
+
+    TEST_CASE( "Check for the type of the returned value by some operators", "[strong type]" )
+    {
+        REQUIRE(std::is_same_v<scalar, std::decay_t<decltype(std::declval<scalar>()++)>>);
+        REQUIRE(std::is_same_v<scalar, std::decay_t<decltype(++std::declval<scalar>())>>);
+        REQUIRE(std::is_same_v<scalar, std::decay_t<decltype(std::declval<scalar>()+=std::declval<scalar>())>>);
+
+        REQUIRE(std::is_same_v<scalar, std::decay_t<decltype(std::declval<scalar>()--)>>);
+        REQUIRE(std::is_same_v<scalar, std::decay_t<decltype(--std::declval<scalar>())>>);
+        REQUIRE(std::is_same_v<scalar, std::decay_t<decltype(std::declval<scalar>()-=std::declval<scalar>())>>);
+
+        REQUIRE(std::is_same_v<scalar, std::decay_t<decltype(std::declval<scalar>()+std::declval<scalar>())>>);
+        REQUIRE(std::is_same_v<scalar, std::decay_t<decltype(std::declval<scalar>()-std::declval<scalar>())>>);
+        REQUIRE(std::is_same_v<scalar, std::decay_t<decltype(std::declval<scalar>()*std::declval<scalar>())>>);
+        REQUIRE(std::is_same_v<scalar, std::decay_t<decltype(std::declval<scalar>()/std::declval<scalar>())>>);
+    }
+} // namespace test_self_arithmetic
+
+namespace test_stringable
+{
+    struct stringable : wit::strong_type<unsigned long long, stringable, wit::stringable>
+    {
+        using strong_type::strong_type;
+    };
+
+    TEST_CASE( "Stringable strong type", "[strong type]")
+    {
+        stringable foo{ 42 };
+        REQUIRE( foo.to_string() == "42" );
+        REQUIRE( std::to_string(foo) == "42" );
+    }
+} // namespace test_stringable
+
+namespace test_convertible
+{
+    struct convertible : wit::strong_type<unsigned long long, convertible, wit::convertible_to_value>
+    {
+        using strong_type::strong_type;
+    };
+
+    TEST_CASE( "Convertible to value strong type", "[strong type]")
+    {
+        convertible foo{ 59 };
+        REQUIRE( (int)foo == 59 );
+    }
+} // namespace test_convertible
+
+namespace test_composition
+{
+    // safe length unit => value is in meter
+    struct meter : wit::strong_type<
+        float           // value type
+        , meter
+        , wit::comparable
+        , wit::self_addable, wit::self_subtractable, wit::signable
+        , wit::incrementable, wit::decrementable
+        >
+    {
+        using strong_type::strong_type;
+    };
+
+    meter operator""_m(long double _meters) { return meter{float(_meters)}; }
+    meter operator""_m(unsigned long long _meters) { return meter{float(_meters)}; }
+
+    namespace test_handmade_composition
+    {
+        // a 3d vector without any semantic
+        template<typename T>
+        struct vector3
+        {
+            vector3(T _x, T _y, T _z) : x{std::move(_x)}, y{std::move(_y)}, z{std::move(_z)} {}
+            bool operator==(const vector3& _rhs) const { return x == _rhs.x && y == _rhs.y && z == _rhs.z; }
+            bool operator!=(const vector3& _rhs) const { return !(*this == _rhs); }
+            vector3 operator+(const vector3& _rhs) const { return { x + _rhs.x, y + _rhs.y, z + _rhs.z }; }
+            T x, y, z;
+        };
+
+        TEST_CASE( "Strong type: vector3<meter>", "[strong_type]" )
+        {
+            auto v0 = vector3<meter>{ 1_m, 2_m, 3_m };
+            auto v1 = vector3<meter>{ 1_m, 2_m, 3_m };
+            auto v2 = vector3<meter>{ 4_m, 5_m, 6_m };
+
+            REQUIRE(v0==v1);
+            REQUIRE(v1!=v2);
+            // REQUIRE(v1+v2 = vector3{ 5_m, 7_m, 9_m } )
+        }
+    }
+
+    // strong type aggregate tentative
+    namespace test_strongly_typed_tuple
+    {
+        template<typename T>
+        struct vector3 : wit::strong_type<std::tuple<T, T, T>, vector3<T>, wit::equalable, wit::self_addable, wit::self_subtractable>
+        {
+            using wit::strong_type<std::tuple<T, T, T>, vector3<T>, wit::equalable, wit::self_addable, wit::self_subtractable>::strong_type;
+        };
+
+        TEST_CASE( "Strong type: experimental::vector3<meter>", "[strong_type]" )
+        {
+            using vec3 = vector3<meter>;
+
+            auto v0 = vec3{ 1_m, 2_m, 3_m };
+            auto v1 = vec3{ 1_m, 2_m, 3_m };
+            auto v2 = vec3{ 3_m, 5_m, 7_m };
+
+            REQUIRE( v0==v1 );
+            REQUIRE( v1!=v2 );
+            REQUIRE( v1+v2 == vec3{ 4_m, 7_m, 10_m });
+            REQUIRE( v2-v1 == vec3{ 2_m, 3_m, 4_m });
+        }
+    } // namespace test_strongly_typed_tuple
+
+    namespace test_strongly_typed_array
+    {
+    } // namespace test_strongly_typed_array
+} // namespace test_composition
