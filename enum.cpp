f4efadbdba15ad62e931ad95a0177993efceef57
DEZAUTEZ Sébastien <drcosinus@free.fr>
2018-05-17 17:15:25 +0200

    numberless enum - fixing compilation with MSVC (it seems typeid does not return a constant expression)

--- enum.cpp
+++ enum.cpp
@@ -1,181 +1,178 @@
 #include <iostream>
 #include <utility>
+#include <typeinfo>
+#include <tuple>
+#include <typeindex>
 
-    /*
 namespace wit
 {
-    // same as strong_type.h
-    namespace detail
+    inline namespace experimental
     {
-        // check if the type `U` is amongst the set of types `Ts`
-        template <typename U, typename... Ts> struct has;
-        template <typename U, typename... Ts> struct has<U, U, Ts...> : std::true_type {};
-        template <typename U, typename T, typename... Ts> struct has<U, T, Ts...> : has<U, Ts...> {};
-        template <typename U> struct has<U> : std::false_type {};
-    } // namespace detail
-
-    struct invalid_enum_t
-    {
-    };
-
-    template<typename... Ts>
-    struct enumeration
-    {
-        // is possible type?
-        template<typename T>
-        static constexpr bool is_assignable_v = detail::has<T, Ts...>::value || std::is_same_v<T, invalid_enum_t>;
-
-        // template<typename T, typename = std::enable_if_t<is_assignable_v<T>>>
-        // static constexpr const T& arg() { return *((const T*)1); }
-
-        template<typename T, typename = std::enable_if_t<is_assignable_v<T>>>
-        struct Enumerator_
+        // all values of the same type are equalivent, identical, equal.
+        // all values of any other types are different
+        template<typename crtp>
+        struct value
         {
+            bool operator==(const crtp&) const { return true;}
+            template<typename T>
+            bool operator==(const T&) const { return false; }
         };
 
-        template<typename T>
-        static constexpr auto Enumerator = Enumerator_<T>{};
-
-        constexpr enumeration()
-        : value_{ &ID<invalid_enum_t> }
+        template<typename crtp>
+        struct model
         {
-        }
-        constexpr enumeration(const enumeration&) = default;
-        constexpr enumeration(enumeration&&) = default;
-        template<typename T, typename = std::enable_if_t<is_assignable_v<T>>>
-        constexpr enumeration(const Enumerator_<T>&)
-        : value_{ &ID<T> }
-        {
-        }
-        enumeration& operator=(invalid_enum_t&)
-        {
-            // ...
-            return *this;
-        }
+            template<typename F>
+            static void forEach(F f)
+            {
+                forEach(f, std::make_index_sequence<std::tuple_size_v<typename crtp::values_type>>{});
+            }
+        protected:
+            template<typename T>
+            static constexpr auto ID() { return &typeid(T); } // typeid does not return a constant expression with MSVC
+        private:
+            template<typename F, std::size_t... INDICES>
+            static constexpr void forEach(F f, std::index_sequence<INDICES...>)
+            {
+                ( f(std::tuple_element_t<INDICES, typename crtp::values_type>{}), ... );
+            }
+        };
+    } // namespace experimental
+} // namespace wit
 
-        template<typename T, typename = std::enable_if_t<is_assignable_v<T>>>
-        void assign()
-        {
-            //static_assert(detail::has<T, Ts...>::value, "Value not allowed");
-            // ...
-            value_ = &ID<T>;
-        }
+using wit::model;
 
-        template<typename T, typename = std::enable_if_t<is_assignable_v<T>>>
-        constexpr bool is() const
-        {
-            return &ID<T> == value_;
-        }
-
-        bool operator==(const enumeration& _rhs) const { return value_==_rhs.value_; }
-    private:
-        template<typename T>
-        static constexpr bool ID = false;
-        
-        const bool* value_;
-    };
-}*/
-
-struct test_enum
+struct test_enum : model<test_enum>
 {
-    // For now only Value addresses matter
-    struct Value final
-    {   // neither copyable nor movable
-        Value(const Value&)=delete;
-        Value(Value&&)=delete;
-        Value& operator=(const Value&)=delete;
-        Value& operator=(Value&&)=delete;
+    struct Invalid : wit::value<Invalid> { };
+    struct ValueTypeA : wit::value<ValueTypeA> { };
+    struct ValueTypeB : wit::value<ValueTypeB> { };
+    struct ValueTypeC : wit::value<ValueTypeC> { };
+    struct ValueTypeD : wit::value<ValueTypeD> { };
 
-        bool operator==(const Value& _rhs) const { return this == &_rhs; }
-        bool operator!=(const Value& _rhs) const { return !(*this == _rhs); }
-    //private:
-    //    friend test_enum;
-        constexpr Value() = default;
-    };
+    using values_type = std::tuple<ValueTypeA, ValueTypeB, ValueTypeC, ValueTypeD>;
 
-    static constexpr Value ValueA;
-    static constexpr Value ValueB;
-    static constexpr Value ValueC;
+    static constexpr ValueTypeA ValueA = {};
+    static constexpr ValueTypeB ValueB = {};
+    static constexpr ValueTypeC ValueC = {};
+    static constexpr ValueTypeD ValueD = {};
 
-    constexpr test_enum() = default;
-    constexpr test_enum(const Value& _value) : value_ptr_{&_value} {} // intentionally implicit
-    constexpr bool operator==(const Value& _value) const { return value_ptr_ == &_value; }
-    constexpr bool operator!=(const Value& _value) const { return !(*this == _value); }
-    constexpr bool operator==(const test_enum& _rhs) const { return value_ptr_==_rhs.value_ptr_; }
-    constexpr bool operator!=(const test_enum& _rhs) const { return !(*this == _rhs); }
+    test_enum() : value_{ ID<Invalid>() } {}
+    test_enum(const ValueTypeA&) : value_{ ID<ValueTypeA>() } {};
+    test_enum(const ValueTypeB&) : value_{ ID<ValueTypeB>() } {};
+    test_enum(const ValueTypeC&) : value_{ ID<ValueTypeC>() } {};
+    test_enum(const ValueTypeD&) : value_{ ID<ValueTypeC>() } {};
 
-    template<typename F>
-    static void forEach(F f)
+    bool operator==(const test_enum& _other) const { return value_==_other.value_; }
+    bool operator!=(const test_enum& _other) const { return !(*this==_other); }
+    bool operator==(const ValueTypeA&) const { return value_==ID<ValueTypeA>(); }
+    bool operator==(const ValueTypeB&) const { return value_==ID<ValueTypeB>(); }
+    bool operator==(const ValueTypeC&) const { return value_==ID<ValueTypeC>(); }
+    bool operator==(const ValueTypeD&) const { return value_==ID<ValueTypeD>(); }
+    bool operator!=(const ValueTypeA& _value) const { return !(*this==_value); }
+    bool operator!=(const ValueTypeB& _value) const { return !(*this==_value); }
+    bool operator!=(const ValueTypeC& _value) const { return !(*this==_value); }
+    bool operator!=(const ValueTypeD& _value) const { return !(*this==_value); }
+
+    friend std::ostream& operator<<(std::ostream& _os, const test_enum& _enum)
     {
-        // fake implementation
-        f(ValueA);
-        f(ValueB);
-        f(ValueC);
+        _os << _enum.value_->name();
+        return _os;
+    }
+
+    friend std::ostream& operator<<(std::ostream& _os, const ValueTypeA&)
+    {
+        _os << typeid(ValueTypeA).name();
+        return _os;
+    }
+    friend std::ostream& operator<<(std::ostream& _os, const ValueTypeB&)
+    {
+        _os << typeid(ValueTypeB).name();
+        return _os;
+    }
+    friend std::ostream& operator<<(std::ostream& _os, const ValueTypeC&)
+    {
+        _os << typeid(ValueTypeC).name();
+        return _os;
     }
 private:
-    const Value* value_ptr_ = nullptr;
+    const std::type_info* value_;
 };
 
-template<typename T, typename U>
-auto CHECK_EQ(const T& e, const U& v)
-{
-    return (e==v) ? "OK" : "-> FAILURE <-";
+std::size_t FailureCount = 0;
+std::size_t CheckCount = 0;
+
+#define __CHECK_OP(__E,__V,__OP,__FILE,__LINE) \
+{ \
+    ++CheckCount; \
+    if (!(__E __OP __V)) \
+    { \
+        ++FailureCount; \
+        cout << __FILE << '(' << __LINE << "): FAILURE: " << #__E << #__OP << #__V << " aka " << __E << #__OP << __V  << endl; \
+    } \
 }
 
-template<typename T, typename U>
-auto CHECK_NE(const T& e, const U& v)
-{
-    return (e!=v) ? "OK" : "-> FAILURE <-";
-}
+#define CHECK_EQ(__E,__V) __CHECK_OP(__E,__V,== ,__FILE__,__LINE__)
+#define CHECK_NE(__E,__V) __CHECK_OP(__E,__V,!= ,__FILE__,__LINE__)
+#define FAIL() cout << __FILE__ << '(' << __LINE__ << "): FAILURE: always" << endl;
 
 int main(void)
 {
     using namespace std;
     {
         test_enum foo;
-        cout << "#01 " << CHECK_NE( foo, test_enum::ValueA ) << endl;
-        cout << "    " << CHECK_NE( foo, test_enum::ValueB ) << endl;
-        cout << "    " << CHECK_NE( foo, test_enum::ValueC ) << endl;
+        CHECK_NE( foo, test_enum::ValueA );
+        CHECK_NE( foo, test_enum::ValueB );
+        CHECK_NE( foo, test_enum::ValueC );
     }
     {
         test_enum foo{ test_enum::ValueA };
-        cout << "#02 " << CHECK_EQ( foo, test_enum::ValueA ) << endl;
-        cout << "    " << CHECK_NE( foo, test_enum::ValueB ) << endl;
-        cout << "    " << CHECK_NE( foo, test_enum::ValueC ) << endl;
+        CHECK_EQ( foo, test_enum::ValueA );
+        CHECK_NE( foo, test_enum::ValueB );
+        CHECK_NE( foo, test_enum::ValueC );
     }
     {
         test_enum foo( test_enum::ValueB );
-        cout << "#03 " << CHECK_NE( foo, test_enum::ValueA ) << endl;
-        cout << "    " << CHECK_EQ( foo, test_enum::ValueB ) << endl;
-        cout << "    " << CHECK_NE( foo, test_enum::ValueC ) << endl;
+        CHECK_NE( foo, test_enum::ValueA );
+        CHECK_EQ( foo, test_enum::ValueB );
+        CHECK_NE( foo, test_enum::ValueC );
     }
     {
         test_enum foo = test_enum::ValueC;
-        cout << "#04 " << CHECK_NE( foo, test_enum::ValueA ) << endl;
-        cout << "    " << CHECK_NE( foo, test_enum::ValueB ) << endl;
-        cout << "    " << CHECK_EQ( foo, test_enum::ValueC ) << endl;
+        CHECK_NE( foo, test_enum::ValueA );
+        CHECK_NE( foo, test_enum::ValueB );
+        CHECK_EQ( foo, test_enum::ValueC );
     }
     {
         test_enum foo1{ test_enum::ValueA }, foo2{ test_enum::ValueA }, foo3{ test_enum::ValueB };
-        cout << "#05 " << CHECK_EQ( foo1, foo2 ) << endl;
-        cout << "    " << CHECK_NE( foo2, foo3 ) << endl;
-        cout << "    " << CHECK_NE( foo1, foo3 ) << endl;
+        CHECK_EQ( foo1, foo2 );
+        CHECK_NE( foo2, foo3 );
+        CHECK_NE( foo1, foo3 );
     }
     {
         test_enum foo;
         foo = test_enum::ValueB;
-        cout << "#06 " << CHECK_EQ( foo, test_enum::ValueB ) << endl;
+        CHECK_EQ( foo, test_enum::ValueB );
     }
     {
         test_enum foo1, foo2{ test_enum::ValueC };
         foo1 = foo2;
-        cout << "#07 " << CHECK_EQ( foo1, foo2 ) << endl;
+        CHECK_EQ( foo1, foo2 );
     }
     {
-        const test_enum::Value* values[] = { &test_enum::ValueA, &test_enum::ValueB, &test_enum::ValueC };
         std::size_t index = 0;
-        cout << "#08 ";
-        test_enum::forEach([&values, &index](const test_enum::Value& _value){ cout << CHECK_EQ(*values[index++], _value) << " "; });
-        cout << endl;
+        test_enum::forEach([&index](const auto& _value)
+        {
+            //cout << typeid(_value).name() << endl;
+            switch(index++)
+            {
+            case 0: CHECK_EQ(_value, test_enum::ValueA); break;
+            case 1: CHECK_EQ(_value, test_enum::ValueB); break;
+            case 2: CHECK_EQ(_value, test_enum::ValueC); break;
+            case 3: CHECK_EQ(_value, test_enum::ValueD); break;
+            default: FAIL(); break;
+            }
+        });
     }
+
+    cout << "Tests: " << (CheckCount-FailureCount) << "/" << CheckCount << endl;
 }
