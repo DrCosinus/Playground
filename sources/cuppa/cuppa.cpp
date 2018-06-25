75ace85c31d6c2a6dc07b169d1258e5a2d54a3f0
DEZAUTEZ Sébastien <drcosinus@free.fr>
2018-06-25 17:23:26 +0200

    wip window creation

--- sources/cuppa/cuppa.cpp
+++ sources/cuppa/cuppa.cpp
@@ -1,5 +1,50 @@
 #include "cuppa.hpp"
 
+#include <windows.h>
+// http://www.winprog.org/tutorial/simple_window.html
+
+static constexpr auto windowClassName = "cuppaWindowClass";
+
+static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch(msg)
+    {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+        break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+[[maybe_unused]]
+static void RegisterWindowClass()
+{
+    WNDCLASSEX wc;
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.style         = 0;
+    wc.lpfnWndProc   = WndProc;
+    wc.cbClsExtra    = 0;
+    wc.cbWndExtra    = 0;
+    //wc.hInstance     = hInstance;
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.lpszMenuName  = NULL;
+    wc.lpszClassName = windowClassName;
+    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+
+    if(!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return;
+    }
+}
+
 namespace cuppa
 {
     void app::run()
