cf5ae9df3184fb1765c1ae381aae1429450cda9f
DrCosinus <minwene@free.fr>
2018-07-21 01:30:33 +0200

    cuppa proto: - creating a console struct

--- sources/cuppa/proto.cpp
+++ sources/cuppa/proto.cpp
@@ -1,29 +1,74 @@
 #include "proto.hpp"
 
+#include <string>
+#include <iostream>
+
+using namespace cuppa;
+
 namespace
 {
-    struct proto : cuppa::app
+    struct proto : app
     {
-        Font font;
+        struct Console
+        {
+            static constexpr std::size_t cols = 80;
+            static constexpr std::size_t rows = 30;
+            std::size_t caret_row = 0;
+            char buffer[cols*rows];
+            Font font;
+            proto* g;
+            std::size_t count = 0;
+
+            Console()
+            {
+            }
+            void setup(proto& _proto)
+            {
+                g = &_proto;
+                font = g->loadFont("Consolas", 14);
+                auto p = g->textSize("1");
+                std::cout << p.width.getAs<float>() << "x" << p.height.getAs<float>() << std::endl;
+                p = g->textSize("12345678901234567890123456789012345678901234567890123456789012345678901234567890\n0");
+                std::cout << p.width.getAs<float>() << "x" << p.height.getAs<float>() << std::endl;
+                writeline("Hello World!");
+                writeline("DrCosinOS");
+            }
+            void draw()
+            {
+                g->textFont(font);
+                g->fill(Green);
+                for(size_t row = 0; row < rows; ++row)
+                {
+                    g->text({buffer+row*cols, cols}, { 0_px, 16_px * row});
+                }
+                count = (count+1)%60;
+                if (count < 30)
+                {
+                    g->rect({ 5_px, 8_px+16_px * caret_row}, { 10_px, 14_px });
+                }
+            }
+            void writeline(std::string_view line)
+            {
+                auto subline = line.substr(0, 40);
+                std::copy(std::begin(subline), std::end(subline), buffer+caret_row*cols);
+                caret_row++;
+            }
+        };
+
+        Console console;
+
         void setup() override
         {
-            size( 640_px, 480_px );
-            font = loadFont("Consolas", 14);
-            textFont(font);
+            size( 800_px, 600_px );
+            console.setup(*this);
         }
+
         void draw() override
         {
+            scale( 780 / 640.f, 580 / 480.f );
+            translate({ 10_px, 10_px });
             background(Black);
-            fill(Green);
-            text("HELLO WORLD!", { 0_px, -3_px});
-            text("IWMO....II00", { 0_px, 12_px});
-            for(int row = 2; row < 32; ++row)
-            {
-                text("012345678901234567890123456789012345678901234567890123456789", { 0_px, 15_px*row-3_px});
-            }
-            // noFill();
-            // stroke(0.5_px, White);
-            // rect({320_px, 240_px}, {640_px, 480_px});
+            console.draw();
 
             rect(pos, { 10_px, 10_px });
 
@@ -46,7 +91,7 @@ namespace
     };
 } // anonymous namespace
 
-std::unique_ptr<cuppa::app> createProto()
+std::unique_ptr<app> createProto()
 {
     return std::make_unique<proto>();
 }
